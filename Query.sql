a65dc21650c5badf08969841dabfd680e9b5286a
student-Imran <ahmedimranmd55@gmail.com>
2026-06-14 17:45:57 +0600

    First Query Done

---
+++ Query.sql
@@ -0,0 +1,13 @@
+/*
+Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.*/
+
+select
+  match_id,
+  fixture,
+  base_ticket_price
+from
+  matches
+where
+  tournament_category = 'Champions League'
+  and match_status = 'Available'
+
